70899470ba227a8621e94a5708916ae0dc386549
Yuri Abramov <9539674+yurinka1@users.noreply.github.com>
2024-12-02 11:11:33 +0200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.21.4
+
+require github.com/snyk-fixtures/private-simple-go v0.0.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	simpleSum "github.com/snyk-fixtures/private-simple-go"
+)
+
+func main() {
+	fmt.Print(simpleSum.Sum(1, 2))
+}
